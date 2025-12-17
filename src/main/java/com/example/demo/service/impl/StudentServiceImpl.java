452ce1caa35f8c91984416c2e2280ec074515508
bala-amypo <bala@amypo.in>
2025-12-17 10:20:05 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -7,6 +7,8 @@ import com.example.demo.entity.Student;
 import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
 
+import java.util.List;
+
 @Service
 public class StudentServiceImpl implements StudentService{
     @Autowired
@@ -21,6 +23,6 @@ public class StudentServiceImpl implements StudentService{
     }
     @GetMapping("getall")
     public List<Student> get(){
-        return stdser
+        return stdser.getAllStudents();
     }
 }
